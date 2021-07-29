6725a92974ef54c4f9e398b0827520864d9eb0ce
klj9236 <87046019+klj9236@users.noreply.github.com>
2021-07-29 19:05:51 -0400

    added validation and input

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -8,8 +8,14 @@ import requests #still need to import even tho its installed. Just do this once
 from dotenv import load_dotenv
 import os
 
+symbol = input("PLEASE ENTER STOCK SYMBOL: ")
+print(symbol)
 
-symbol = "MSFT"
+#validation symbol
+if (len(symbol)>=1<=5):
+    print("VALID SYMBOL")
+
+
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}&datatype=csv"
@@ -28,6 +34,17 @@ recent_low = price_df["low"].min()
 recent_high = price_df["high"].max()
 latest_close = price_df.iloc[0,4]
 
+
+
+#below code from https://www.programiz.com/python-programming/datetime/timestamp-datetime
+from datetime import datetime
+# current date and time
+now = datetime.now()
+
+timestamp = datetime.timestamp(now)
+dt_object = datetime.fromtimestamp(timestamp)
+
+
 import pandas as pd
 
 df = pd.DataFrame(price_df, columns=["timestamp", "open", "high", "low", "close", "volume"])
@@ -35,11 +52,14 @@ df = pd.DataFrame(price_df, columns=["timestamp", "open", "high", "low", "close"
 df.to_csv("data/prices.csv", index=False)
 
 
+
+
+
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print(f"SELECTED SYMBOL: {(symbol.upper())}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print(f"REQUEST MADE AT: {(dt_object)}")
 print("-------------------------")
 print(f"LATEST DAY: {(latest_day)}")
 print(f"LATEST CLOSE: {to_usd(latest_close)}")
