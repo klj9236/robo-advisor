52cb1d68023209c287abac9333823921fba8fd40
klj9236 <87046019+klj9236@users.noreply.github.com>
2021-07-29 21:35:38 -0400

    added recommendation criteria

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -11,20 +11,26 @@ import os
 symbol = input("PLEASE ENTER STOCK SYMBOL: ")
 print(symbol)
 
-#validation symbol
-if (len(symbol)>=1<=5 and symbol.isalpha()):
-    print("VALID SYMBOL")
-else:
-    print("INVALID SYMBOL")
-    quit()
-
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}&datatype=csv"
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={api_key}&datatype=csv"
 
 from pandas import read_csv
 price_df = read_csv(request_url) 
 
+#validation symbol
+#if symbol not in price_df:
+#    print("SORRY SYMBOL NOT FOUND")
+
+if (len(symbol)>=1<=5 and symbol.isalpha()):
+    print("VALID SYMBOL")
+
+#elif symbol not in price_df:
+#    print("SORRY SYMBOL NOT FOUND")
+
+else: 
+    print("INVALID SYMBOL")
+    quit()
 
 latest_day = (price_df.iloc[0,0])
 
@@ -35,6 +41,7 @@ def to_usd(my_price):
 recent_low = price_df["low"].min()
 recent_high = price_df["high"].max()
 latest_close = price_df.iloc[0,4]
+average_price = price_df["close"].mean()
 
 
 
@@ -67,9 +74,13 @@ print(f"LATEST DAY: {(latest_day)}")
 print(f"LATEST CLOSE: {to_usd(latest_close)}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print(f"RECENT LOW: {to_usd(float(recent_low))}")
+print(f"AVERAGE PRICE: {to_usd(float(average_price))}")
 print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
+if float(average_price)>float(latest_close):
+    print("RECOMMENDATION: BUY!")
+elif float(average_price)<float(latest_close):
+    print("RECOMMENDATION: SELL!")
+print("RECOMMENDATION REASON: IF LATEST CLOSE IS ABOVE 100 DAY AVERAGE, SELL! IF LATEST CLOSE IS BELOW 100 DAY AVERAGE, BUY!")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
